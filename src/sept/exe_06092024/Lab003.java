492cad293f5662ccab554b40f5204c37df3c35f1
Admin <khushbuzalavadiya27@gmail.com>
2024-09-17 10:29:37 -0400

    simple

---
+++ src/sept/exe_06092024/Lab003.java
@@ -0,0 +1,7 @@
+package sept.exe_06092024;
+
+public  class Lab003 {
+    public static void main(String args[]) {
+        System.out.println("hi my name is khushbu");
+    }
+}
---
+++ src/sept/exe_06092024/Lab004.java
@@ -0,0 +1,14 @@
+package sept.exe_06092024;
+
+public class Lab004 {
+       public static void main (String args[]){
+           int a=10;
+           int b=20;
+           int c;
+           System.out.println(a+b);
+           System.out.println(a-b);
+           System.out.println(a*b);
+           System.out.println(a/b);
+           System.out.println(a%b);
+       }
+}
