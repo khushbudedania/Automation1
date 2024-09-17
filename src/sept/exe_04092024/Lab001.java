f8deeb3e7126862cc668997088525663a40ef085
Admin <khushbuzalavadiya27@gmail.com>
2024-09-17 10:30:54 -0400

    simple

--- src/sept/exe_04092024/Lab001.java
+++ src/sept/exe_04092024/Lab001.java
@@ -1,4 +1,8 @@
 package sept.exe_04092024;
 
 public class Lab001 {
-}
+    public static void main(String args[])
+    {
+            System.out.println("Hello Khushbu");
+        }
+    }
--- src/sept/exe_04092024/Lab002.java
+++ src/sept/exe_04092024/Lab002.java
@@ -1,4 +1,7 @@
 package sept.exe_04092024;
 
 public class Lab002  {
+    public static void main(String[] khushbu){
+        System.out.println("today's weather");
+    }
 }
