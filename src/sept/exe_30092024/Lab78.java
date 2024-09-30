2fd1ddbeebbacd5b4c3afaa77b2fab98f817366d
Admin <khushbuzalavadiya27@gmail.com>
2024-09-30 14:54:52 -0400

    30th sep.

---
+++ src/sept/exe_30092024/Lab78.java
@@ -0,0 +1,17 @@
+package sept.exe_30092024;
+
+public class Lab78 {
+    public static void main(String[] args)
+    {
+       String name = function_type2();
+       System.out.println(name);
+
+    }
+
+    public static String function_type2()
+    {
+        System.out.println(" with Return type but without Argument.");
+        return "khushbu";
+    }
+}
+// 2) with Return type but without Argument.
---
+++ src/sept/exe_30092024/Lab79.java
@@ -0,0 +1,14 @@
+package sept.exe_30092024;
+
+public class Lab79 {
+    public static void main(String[] args) {
+       Function_type3("khushbu");
+
+    }
+    public static void Function_type3(String name)
+    {
+        System.out.println("without return type but with argument.");
+        System.out.println("your name is " +name);
+    }
+}
+// 3) without return type but with argument.
